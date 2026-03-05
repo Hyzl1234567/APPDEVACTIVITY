2c8d8f44e0ec423f4373d0684189732eb4a8e23b
Hyzl1234567 <llloll123pll@gmail.com>
2026-03-05 18:16:43 +0800

    Initial commit

--- src/navigations/AuthNav.js
+++ src/navigations/AuthNav.js
@@ -2,6 +2,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { ROUTES } from '../utils';
 
 import Login from '../screens/auth/Login';
+import Register from '../screens/auth/Register';
 import HomeScreen from '../screens/HomeScreen';
 
 const Stack = createStackNavigator();
@@ -11,6 +12,7 @@ const AuthNavigation = () => {
     <Stack.Navigator initialRouteName={ROUTES.LOGIN}>
       <Stack.Screen name={ROUTES.LOGIN} component={Login} />
       <Stack.Screen name={ROUTES.HOME} component={HomeScreen} />
+      <Stack.Screen name={ROUTES.REGISTER} component={Register} />
     </Stack.Navigator>
   );
 };
