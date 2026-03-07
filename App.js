503e5038ab1037a0a1b72ae8747da3b69f5da5c3
Hyzl1234567 <llloll123pll@gmail.com>
2026-03-07 18:41:25 +0800

    Updated login and register features

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
-import 'react-native-gesture-handler';
 import React from 'react';
 import { View } from 'react-native';
 
 import AppNavigationNi from './src/navigations';
 
+import rootSaga from './src/app/sagas';
+import configureStore from './src/app/reducers';
+
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const { store, persistor, runSaga } = configureStore();
+runSaga(rootSaga);
+
 const App = () => {
   return (
-    <View style={{ flex: 1 }}>
-      <AppNavigationNi />
-    </View>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <View style={{ flex: 1 }}>
+          <AppNavigationNi />
+        </View>
+      </PersistGate>
+    </Provider>
   );
 };
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,2 @@
-export { default as ROUTES } from './routes'
-export { default as IMG } from './image'
+export { default as ROUTES } from './routes';
+export { default as IMG } from './image';
